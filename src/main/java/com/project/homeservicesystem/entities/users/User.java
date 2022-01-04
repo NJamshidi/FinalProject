ff763e0e9654d5f976bd244f296e791d3d81265b
unknown <narges40jam67@gmail.com>
2022-01-05 00:08:45 +0330

    add userclass as superclass

--- src/main/java/com/project/homeservicesystem/entities/users/User.java
+++ src/main/java/com/project/homeservicesystem/entities/users/User.java
@@ -1,2 +1,26 @@
-package com.project.homeservicesystem.entities.users;public class User {
-}
+package com.project.homeservicesystem.entities.users;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    @Setter
+    @Builder
+    @MappedSuperclass
+    public class User {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+        private String firstName;
+        private String lastName;
+        @Column(nullable = false, unique = true)
+        private String email;
+        private String password;
+        private Date registerDate = new Date();
+        private double credit;
+    }
+
