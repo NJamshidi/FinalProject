c06b7083aaccce73b9af0d357f48f206c80106d8
unknown <narges40jam67@gmail.com>
2022-01-05 01:11:58 +0330

    add userfeedback class

---
+++ src/main/java/com/project/homeservicesystem/entities/users/UserFeedback.java
@@ -0,0 +1,25 @@
+package com.project.homeservicesystem.entities.users;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class UserFeedback {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String text;
+    @ManyToOne
+    private Customer customer;
+    @ManyToOne
+    private Provider provider;
+    private int rate;
+}
