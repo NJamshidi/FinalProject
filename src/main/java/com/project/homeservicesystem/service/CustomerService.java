1021d5b24bfd4a2261d402db00ea8f84e7779c03
unknown <narges40jam67@gmail.com>
2022-01-05 22:54:02 +0330

    add save new customer and find customer by username and password in customsrvice

--- src/main/java/com/project/homeservicesystem/service/CustomerService.java
+++ src/main/java/com/project/homeservicesystem/service/CustomerService.java
@@ -1,4 +1,20 @@
 package com.project.homeservicesystem.service;
 
+import com.project.homeservicesystem.dao.CustomerDao;
+import com.project.homeservicesystem.entities.users.Customer;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CustomerService {
+    private CustomerDao customerDao;
+    public void saveNewCustomer(Customer customer) {
+        customerDao.save(customer);
+    }
+    public Customer findCustomerByUserNameAndPass(String userName, String password) {
+        return customerDao.findByUserNameAndPass(userName,password);
+    }
+
+
 }
