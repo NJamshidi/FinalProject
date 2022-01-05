85b9d1f8c6afe0752f901aaee8dad1f04c31e15d
unknown <narges40jam67@gmail.com>
2022-01-05 16:42:36 +0330

    add crud methods for providerdao

--- src/main/java/com/project/homeservicesystem/dao/ProviderDao.java
+++ src/main/java/com/project/homeservicesystem/dao/ProviderDao.java
@@ -1,4 +1,53 @@
 package com.project.homeservicesystem.dao;
 
+import com.project.homeservicesystem.entities.users.Provider;
+import com.project.homeservicesystem.entities.users.User;
+import com.project.homeservicesystem.util.HibernateUtil;
+import com.project.homeservicesystem.util.ImageWrapper;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
 public class ProviderDao {
+    private SessionFactory sessionFactory = HibernateUtil.buildSessionFactory();
+
+
+    public void save(Provider provider) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        ImageWrapper.saveImage(provider.getFirstName()+provider.getLastName()+".jpg",provider);
+        session.save(provider);
+        transaction.commit();
+        session.close();
+    }
+
+    public void update(Provider provider) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.update(provider);
+        transaction.commit();
+        session.close();
+    }
+
+    public void delete(Provider provider) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(provider);
+        transaction.commit();
+        session.close();
+    }
+
+    public List<User> findAll() {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Query query = session.createQuery("from Provider ");
+        List<User> users = query.list();
+        transaction.commit();
+        session.close();
+        return users;
+    }
+
 }
