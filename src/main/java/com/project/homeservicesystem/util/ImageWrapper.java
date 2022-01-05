ff0e9101172719f7ca8d88d8d42a079522429730
unknown <narges40jam67@gmail.com>
2022-01-05 11:24:08 +0330

    add ImageWrapper class

---
+++ src/main/java/com/project/homeservicesystem/util/ImageWrapper.java
@@ -0,0 +1,29 @@
+package com.project.homeservicesystem.util;
+
+import com.project.homeservicesystem.entities.users.Provider;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+public class ImageWrapper {
+    private int maxFileSize =350 * 350;
+
+    public static void saveImage(String picName, Provider provider) {
+        File file = new File("C:\\image\\" + picName + "");
+        int length = (int) file.length();
+        if (length < 15000 * 15000) {
+            byte[] imageData = new byte[length];
+            try {
+                FileInputStream fileInputStream = new FileInputStream(file);
+                fileInputStream.read(imageData);
+                fileInputStream.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            provider.setImage(imageData);
+        } else
+            throw new RuntimeException("image is very large for upload");
+    }
+
+
+}
