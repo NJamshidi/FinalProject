61a0a768ef7461ba3e2232b76e1dc1a55d93cc77
unknown <narges40jam67@gmail.com>
2022-01-05 22:59:38 +0330

    add save new provider to providerservice

--- src/main/java/com/project/homeservicesystem/service/ProviderService.java
+++ src/main/java/com/project/homeservicesystem/service/ProviderService.java
@@ -1,4 +1,16 @@
 package com.project.homeservicesystem.service;
 
+import com.project.homeservicesystem.dao.ProviderDao;
+import com.project.homeservicesystem.entities.users.Provider;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ProviderService {
+    private ProviderDao providerDao;
+    public void saveNewProvider(Provider provider) {
+        providerDao.save(provider);
+    }
+
 }
