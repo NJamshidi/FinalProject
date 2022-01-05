024ab804399fae9901cb8bddff19493a9c2a4174
unknown <narges40jam67@gmail.com>
2022-01-05 16:30:19 +0330

    add crud method in admindao

--- src/main/java/com/project/homeservicesystem/dao/AdminDao.java
+++ src/main/java/com/project/homeservicesystem/dao/AdminDao.java
@@ -1,4 +1,61 @@
 package com.project.homeservicesystem.dao;
 
+import com.project.homeservicesystem.entities.users.Admin;
+import com.project.homeservicesystem.entities.users.User;
+import com.project.homeservicesystem.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
 public class AdminDao {
+    private SessionFactory sessionFactory = HibernateUtil.buildSessionFactory();
+
+    public void save(Admin admin) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(admin);
+        transaction.commit();
+        session.close();
+    }
+
+    public void update(Admin admin) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.update(admin);
+        transaction.commit();
+        session.close();
+    }
+
+    public void delete(Admin admin) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(admin);
+        transaction.commit();
+        session.close();
+    }
+
+    public List<User> findAll() {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Query query = session.createQuery("from Admin ");
+        List<User> users = query.list();
+        transaction.commit();
+        session.close();
+        return users;
+    }
+    public Admin findByUseAndPass(String userName, String password) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Query query = session.createQuery("From Admin A Where A.passWord = :password and  A.userName=:username");
+        query.setParameter("username", userName);
+        query.setParameter("password", password);
+        Admin admin = (Admin) query.uniqueResult();
+        transaction.commit();
+        session.close();
+        return admin;
+    }
+
 }
