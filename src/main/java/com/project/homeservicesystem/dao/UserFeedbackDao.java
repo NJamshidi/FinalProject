03a827bf68684340ba516ea7e8d2e2d8b0905aa2
unknown <narges40jam67@gmail.com>
2022-01-06 21:40:27 +0330

    add save in userfeedback dao

--- src/main/java/com/project/homeservicesystem/dao/UserFeedbackDao.java
+++ src/main/java/com/project/homeservicesystem/dao/UserFeedbackDao.java
@@ -1,4 +1,22 @@
 package com.project.homeservicesystem.dao;
 
+import com.project.homeservicesystem.entities.users.UserFeedback;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
 public class UserFeedbackDao {
+    private final SessionFactory sessionFactory;
+
+    public void save(UserFeedback userFeedback) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(userFeedback);
+        transaction.commit();
+        session.close();
+    }
 }
