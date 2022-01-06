a0a3b6e4ddaa76cea319f3a04d1dac2498a62798
unknown <narges40jam67@gmail.com>
2022-01-07 01:36:23 +0330

    add addoffertorequest method in serviceofferservice

--- src/main/java/com/project/homeservicesystem/service/ServiceOfferService.java
+++ src/main/java/com/project/homeservicesystem/service/ServiceOfferService.java
@@ -1,12 +1,15 @@
 package com.project.homeservicesystem.service;
 
 import com.project.homeservicesystem.dao.ServiceOfferDao;
+import com.project.homeservicesystem.entities.services.ServiceCategory;
 import com.project.homeservicesystem.entities.services.ServiceOffer;
-import com.project.homeservicesystem.entities.users.Provider;
+import com.project.homeservicesystem.enumaration.ServiceRequestStatus;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 @Getter
 @Setter
@@ -16,5 +19,17 @@ public class ServiceOfferService {
     private ServiceOfferDao serviceOfferDao;
     public void saveNewServiceOffer(ServiceOffer serviceOffer) {
         serviceOfferDao.save(serviceOffer);
+    }
+
+
+    public void addOfferToRequest(ServiceOffer serviceOffer) {
+        Set<ServiceCategory> providerServices = serviceOffer.getProvider().getServices();
+        ServiceCategory serviceCategory = serviceOffer.getServiceRequest().getServiceCategory();
+        if (providerServices.contains(serviceCategory)) {
+            saveNewServiceOffer(serviceOffer);
+            serviceOffer.getServiceRequest().setStatus(ServiceRequestStatus.UNDER_SELECTION);
+        } else {
+            throw new RuntimeException("this request service is not in your selection.");
+        }
     }
 }
