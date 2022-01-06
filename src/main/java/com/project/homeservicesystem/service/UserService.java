5cf0f95c4d664c31d4e0d9e259b3bd1db9e1a35c
unknown <narges40jam67@gmail.com>
2022-01-06 13:14:24 +0330

    add save and finduser in userservice

--- src/main/java/com/project/homeservicesystem/service/UserService.java
+++ src/main/java/com/project/homeservicesystem/service/UserService.java
@@ -1,4 +1,20 @@
 package com.project.homeservicesystem.service;
 
+import com.project.homeservicesystem.dao.ServiceOfferDao;
+import com.project.homeservicesystem.dao.UserDao;
+import com.project.homeservicesystem.entities.services.ServiceOffer;
+import com.project.homeservicesystem.entities.users.User;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class UserService {
+    private UserDao userDao;
+    public void saveNewUser(User user) {
+        userDao.save(user);
+    }
+    public User findUserByUserNameAndPass(String username, String password) {
+        return userDao.findUserByUserNameAndPass(username, password);
+    }
 }
--- src/main/java/com/project/homeservicesystem/service/serviceRequestService.java
+++ src/main/java/com/project/homeservicesystem/service/serviceRequestService.java
@@ -3,7 +3,11 @@ package com.project.homeservicesystem.service;
 import com.project.homeservicesystem.dao.ServiceRequestDao;
 import com.project.homeservicesystem.entities.services.ServiceOffer;
 import com.project.homeservicesystem.entities.services.ServiceRequest;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class serviceRequestService {
     private ServiceRequestDao serviceRequestDao;
     public void saveNewServiceRequest(ServiceRequest serviceRequest) {
