68703e165b9c76cc9008ac3f4599853f0f4678df
unknown <narges40jam67@gmail.com>
2022-01-10 12:09:27 +0330

    add addexpert to subserviceservice

--- src/main/java/ir/maktab/homeservicesystem/service/SubServiceService.java
+++ src/main/java/ir/maktab/homeservicesystem/service/SubServiceService.java
@@ -2,11 +2,14 @@ package ir.maktab.homeservicesystem.service;
 
 import ir.maktab.homeservicesystem.data.dao.SubServiceDao;
 import ir.maktab.homeservicesystem.data.entities.services.SubService;
+import ir.maktab.homeservicesystem.data.entities.users.Expert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
+import java.util.Set;
 
 @Service
 //@RequiredArgsConstructor
@@ -26,4 +29,21 @@ public class SubServiceService extends BaseService<SubService, Integer> {
     public List<SubService> findByMainServiceId(int id) {
         return subServiceDao.findByMainServiceId(id);
     }
+    @Transactional
+    public SubService addExpert(int subServiceId, int expertId) {
+        SubService subService = subServiceDao.getById(subServiceId);
+        Expert expert = expertService.findById(expertId);
+
+        Set<Expert> experts = subService.getExpert();
+        experts.add(expert);
+        subService.setExpert(experts);
+
+        Set<SubService> subServices = expert.getSubService();
+        subServices.add(subService);
+        expert.setSubService(subServices);
+
+        expertService.update(expert);
+        return super.update(subService);
+    }
+
 }
