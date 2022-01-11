61afda91fec33b3d02b504423392cc77bd2dd86e
unknown <narges40jam67@gmail.com>
2022-01-11 14:25:25 +0330

    add acceptoffer to orderservice

--- src/main/java/ir/maktab/homeservicesystem/service/OrderService.java
+++ src/main/java/ir/maktab/homeservicesystem/service/OrderService.java
@@ -1,9 +1,14 @@
 package ir.maktab.homeservicesystem.service;
 
 import ir.maktab.homeservicesystem.data.dao.OrderDao;
+import ir.maktab.homeservicesystem.data.entities.Offer;
 import ir.maktab.homeservicesystem.data.entities.Order;
+import ir.maktab.homeservicesystem.data.enumaration.OfferStatus;
+import ir.maktab.homeservicesystem.data.enumaration.OrderStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import javax.annotation.PostConstruct;
 
 @Service
@@ -19,5 +24,12 @@ public class OrderService extends BaseService<Order, Integer> {
     public void init() {
         setJpaRepository(orderDao);
     }
-
+    @Transactional
+    public Order acceptOffer(Order order) {
+        Offer acceptOffer = order.getAcceptedOffer();
+        acceptOffer.setOfferStatus(OfferStatus.ACCEPTED);
+        order.setAcceptedOffer(acceptOffer);
+       order.setStatus(OrderStatus.ON_GOING);
+        return super.update(order);
+    }
 }
