d7841fe23045e6994b4ba0ffd74319b99b48cbb7
unknown <narges40jam67@gmail.com>
2022-01-12 02:22:53 +0330

    add admin and customer test

--- src/test/java/ir/maktab/homeservicesystem/service/AdminServiceTest.java
+++ src/test/java/ir/maktab/homeservicesystem/service/AdminServiceTest.java
@@ -1,9 +1,15 @@
 package ir.maktab.homeservicesystem.service;
 
 import ir.maktab.homeservicesystem.data.entities.users.Admin;
+import ir.maktab.homeservicesystem.exception.DuplicateInformationException;
+import ir.maktab.homeservicesystem.exception.IncorrectInformationException;
+import ir.maktab.homeservicesystem.exception.NotFoundObjectException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
@@ -12,14 +18,131 @@ class AdminServiceTest {
     private AdminService adminService;
 
     @Test
-    void test_save() {
+    void save() {
         Admin admin = new Admin();
         admin.setFirstName("narges");
         admin.setLastName("jam");
         admin.setEmail("narges@mail.com");
         admin.setPassword("123asd45");
+        assertNotNull(adminService.save(admin));
+    }
+    @Test
+    void saveWithPassJustAlphabet() {
+        Admin admin = new Admin();
+        admin.setFirstName("ali");
+        admin.setLastName("alavi");
+        admin.setEmail("ali@mail.com");
+        admin.setPassword("aaaaaaaaa");
+        assertThrows(IncorrectInformationException.class, () -> adminService.save(admin));
+    }
+    @Test
+    void saveWithPassJustNumber() {
+        Admin admin = new Admin();
+        admin.setFirstName("ahmad");
+        admin.setLastName("ahmadi");
+        admin.setEmail("ahmad@mail.com");
+        admin.setPassword("12312345");
+        assertThrows(IncorrectInformationException.class, () -> adminService.save(admin));
+    }
+    @Test
+    void SaveWithPassLess8Char() {
+        Admin admin = new Admin();
+        admin.setFirstName("narges");
+        admin.setLastName("jamshidi");
+        admin.setEmail("narges@mail.com");
+        admin.setPassword("123a");
+        assertThrows(IncorrectInformationException.class, () -> adminService.save(admin));
+    }
+    @Test
+    void SaveWithDuplicateEmail() {
+        Admin admin = new Admin();
+        admin.setFirstName("nahal");
+        admin.setLastName("razavi");
+        admin.setEmail("narges@mail.com");
+        admin.setPassword("123qwe45");
+        assertThrows(DuplicateInformationException.class, () -> adminService.save(admin));
+    }
 
-        Admin result = adminService.save(admin);
+    @Test
+    void changePass() {
+        Admin admin = adminService.changeAdminPassword(1, "123asd45", "456asd78");
+        assertEquals("456asd78", admin.getPassword());
+    }
+
+    @Test
+    void ChangePassWithIncorrectOldPass() {
+        assertThrows(IncorrectInformationException.class, () ->
+                adminService.changeAdminPassword(1, "123asd45", "456asd78")
+        );
+    }
+
+    @Test
+    void changePassWithInvalidNewPass() {
+        assertThrows(IncorrectInformationException.class, () ->
+                adminService.changeAdminPassword(1, "456asd78", "123")
+        );
+    }
+
+
+    @Test
+    void update() {
+        int id = 3;
+        Admin admin = new Admin();
+        admin.setId(id);
+        admin.setFirstName("nima");
+        admin.setLastName("nimaie");
+        admin.setEmail("nima@mail.com");
+        admin.setPassword("123asd45");
+        assertEquals("nima", adminService.update(admin).getFirstName());
+    }
+
+    @Test
+    void updateWithDuplicateEmail() {
+        int id = 3;
+        Admin admin = new Admin();
+        admin.setId(id);
+        admin.setFirstName("reza");
+        admin.setLastName("rezaie");
+        admin.setEmail("ali@mail.com");
+        admin.setPassword("123abd47");
+        assertThrows(DuplicateInformationException.class, () -> adminService.update(admin));
+    }
+    @Test
+    void loadByEmail() {
+        String email = "narges@mail.com";
+        assertEquals("narges@mail.com",adminService.findAdminByEmail(email).getEmail());
+    }
+    @Test
+    void loadByIdExist() {
+        int id = 1;
+        Admin result = adminService.findById(id);
         assertNotNull(result);
+    }
+
+    @Test
+    void loadByIdNotExist() {
+        int id = 20;
+        assertThrows(NotFoundObjectException.class, () -> adminService.findById(id));
+    }
+
+    @Test
+    void findAll() {
+        List<Admin> adminList = adminService.findAll();
+        assertEquals(3, adminList.size());
+    }
+
+    @Test
+    void delete() {
+        int id = 5;
+        adminService.removeById(id);
+
+        List<Admin> adminList = adminService.findAll();
+        assertEquals(2, adminList.size());
+    }
+
+    @Test
+    void deleteNotExist() {
+        int id = 20;
+        assertThrows(EmptyResultDataAccessException.class, () -> adminService.removeById(id));
     }
 }
---
+++ src/test/java/ir/maktab/homeservicesystem/service/CustomerServiceTest.java
@@ -0,0 +1,75 @@
+package ir.maktab.homeservicesystem.service;
+
+import ir.maktab.homeservicesystem.data.entities.users.Customer;
+import ir.maktab.homeservicesystem.exception.IncorrectInformationException;
+import ir.maktab.homeservicesystem.exception.NotEnoughException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+
+public class CustomerServiceTest {
+    @Autowired
+    private CustomerService customerService;
+
+    @Test
+    void save() {
+        Customer customer = new Customer();
+        customer.setFirstName("asal");
+        customer.setLastName("ahmadi");
+        customer.setEmail("ahmadi@gmail.com");
+        customer.setPassword("12345678asd");
+        customer.setCredit(50000d);
+        Customer result = customerService.save(customer);
+        assertNotNull(result);
+    }
+    @Test
+    void loadById(){
+        Customer customer = customerService.findById(1);
+        System.out.println( customer.getAddress().getPlaque());
+    }
+    @Test
+    void changePass() {
+        Customer customer = customerService.changePassword(1, "12345678asd", "456asd");
+        assertEquals("456asd", customer.getPassword());
+    }
+
+    @Test
+    void changePasswordWithIncorrectOldPass() {
+        assertThrows(IncorrectInformationException.class, () ->
+                customerService.changePassword(1, "123yy4", "45yyy78")
+        );
+    }
+
+    @Test
+    void changePasswordWithIncorrectNewPass() {
+        assertThrows(IncorrectInformationException.class, () ->
+                customerService.changePassword(1, "456asd", "1")
+        );
+    }
+
+
+
+    @Test
+    void increaseCredit() {
+        Customer customer = customerService.increaseCredit(1, 5000.0);
+        assertEquals(55000.0, customer.getCredit());
+    }
+
+    @Test
+    void decreaseCreditOk() {
+        Customer customer = customerService.decreaseCredit(1, 5000.0);
+        assertEquals(10000.0, customer.getCredit());
+    }
+
+    @Test
+    void decreaseCreditNotEnough() {
+        assertThrows(NotEnoughException.class, () -> customerService.decreaseCredit(1, 15000.0));
+    }
+
+}
