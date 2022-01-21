0ca4822ac295deaaa6f4ff1e3c1557db6ba784f0
unknown <narges40jam67@gmail.com>
2022-01-21 03:47:31 +0330

    create controller package and add spring-devtools to pom

---
+++ src/main/java/ir/maktab/homeservicesystem/controller/AdminController.java
@@ -0,0 +1,13 @@
+package ir.maktab.homeservicesystem.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/admins")
+public class AdminController {
+
+
+    }
---
+++ src/main/java/ir/maktab/homeservicesystem/controller/OfferController.java
@@ -0,0 +1,4 @@
+package ir.maktab.homeservicesystem.controller;
+
+public class OfferController {
+}
