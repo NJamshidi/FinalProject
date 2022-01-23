f93683fb3085b75e81d94446fd6ce4d389712d3b
unknown <narges40jam67@gmail.com>
2022-01-24 01:08:44 +0330

    add mainservicelist class and modify mainservice class

---
+++ src/main/java/ir/maktab/homeservicesystem/data/entities/services/MainServiceList.java
@@ -0,0 +1,26 @@
+package ir.maktab.homeservicesystem.data.entities.services;
+
+
+import ir.maktab.homeservicesystem.dto.MainServiceDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MainServiceList {
+    private List<MainServiceDto> mainServiceDtos;
+
+    public void addMainServiceDto(MainServiceDto mainServiceDto) {
+        if (mainServiceDtos == null) {
+            mainServiceDtos = new ArrayList<>();
+        }
+        mainServiceDtos.add(mainServiceDto);
+    }
+}
--- src/main/java/ir/maktab/homeservicesystem/service/MainServiceService.java
+++ src/main/java/ir/maktab/homeservicesystem/service/MainServiceService.java
@@ -1,24 +1,57 @@
 package ir.maktab.homeservicesystem.service;
 
-import ir.maktab.homeservicesystem.data.dao.CustomerDao;
 import ir.maktab.homeservicesystem.data.dao.MainServiceDao;
 import ir.maktab.homeservicesystem.data.entities.services.MainService;
+import ir.maktab.homeservicesystem.data.entities.services.MainServiceList;
+import ir.maktab.homeservicesystem.dto.MainServiceDto;
+import ir.maktab.homeservicesystem.dto.mapper.MainServiceMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import javax.annotation.PostConstruct;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 
 @Service
-//@RequiredArgsConstructor
-public class MainServiceService extends BaseService<MainService, Integer> {
+@RequiredArgsConstructor
+public class MainServiceService {
     private final MainServiceDao mainServiceDao;
-    @Autowired
-    public MainServiceService(MainServiceDao mainServiceDao) {
-        this.mainServiceDao = mainServiceDao;
+    private MainServiceMapper mainServiceMapper;
+
+    @Transactional(readOnly = true)
+    public MainService loadById(int id) {
+        return mainServiceDao.getById(id);
     }
-    @PostConstruct
-    public void init() {
-        setJpaRepository(mainServiceDao);
+
+    @Transactional(readOnly = true)
+    public MainServiceDto loadByIdReturnDto(int id) {
+        MainService mainService = mainServiceDao.getById(id);
+        return mainServiceMapper.toDto(mainService);
+
+    }
+
+    @Transactional
+    public MainServiceDto saveMainService(MainServiceDto mainServiceDto) {
+        MainService mainService = new MainService();
+        mainService.setName(mainServiceDto.getName());
+        MainService saveResult = mainServiceDao.save(mainService);
+        return mainServiceMapper.toDto(saveResult);
+    }
+
+    @Transactional
+    public MainServiceDto updateMainService(MainServiceDto mainServiceDto) {
+        MainService mainService = mainServiceDao.getById(mainServiceDto.getId());
+        mainService.setName(mainServiceDto.getName());
+        MainService saveResult = mainServiceDao.save(mainService);
+        return mainServiceMapper.toDto(saveResult);
+    }
+
+
+    @Transactional(readOnly = true)
+    public MainServiceList loadAll() {
+        List<MainService> mainServices = mainServiceDao.findAll();
+        MainServiceList mainServiceList = new MainServiceList();
+        mainServices.forEach((c) -> mainServiceList.addMainServiceDto(mainServiceMapper.toDto(c)));
+        return mainServiceList;
     }
 }
