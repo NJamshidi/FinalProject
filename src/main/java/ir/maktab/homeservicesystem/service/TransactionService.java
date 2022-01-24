3993989bf9686811b169e219f85d6fe375b5b411
unknown <narges40jam67@gmail.com>
2022-01-24 09:53:28 +0330

    add TransactionDto and TransactionMapper

--- src/main/java/ir/maktab/homeservicesystem/service/TransactionService.java
+++ src/main/java/ir/maktab/homeservicesystem/service/TransactionService.java
@@ -2,22 +2,18 @@ package ir.maktab.homeservicesystem.service;
 
 import ir.maktab.homeservicesystem.data.dao.TransactionDao;
 import ir.maktab.homeservicesystem.data.entities.Transaction;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
 
 @Service
-//@RequiredArgsConstructor
-public class TransactionService extends BaseService<Transaction, Integer> {
+@RequiredArgsConstructor
+public class TransactionService {
     private final TransactionDao transactionDao;
-    @Autowired
-    public TransactionService(TransactionDao transactionDao) {
-        this.transactionDao =transactionDao;
-    }
-    @PostConstruct
-    public void init() {
-        setJpaRepository(transactionDao);
-    }
+    private final CustomerService customerService;
+
 
 }
